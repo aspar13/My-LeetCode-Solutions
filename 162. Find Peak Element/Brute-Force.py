0fa7f4af2ae72b4040b737684a619892766152da
aspar <111035307+aspar13@users.noreply.github.com>
2023-05-19 06:14:02 +0530

    Add files via upload

---
+++ 162. Find Peak Element/Brute-Force.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+
+        for i in range(len(nums)):
+
+            if(i==0 or nums[i] >= nums[i-1]) and (i == len(nums)-1 or nums[i] >= nums[i+1]):
+                return i
